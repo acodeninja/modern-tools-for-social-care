04baf1a40d0c26a8129c5d3ad91939a3400c43d9
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-02 14:07:04 +0000

    Add lambda handler for the search action

--- src/services/search/actions/search.ts
+++ src/services/search/actions/search.ts
@@ -1,5 +1,6 @@
 import {SearchResult} from "../domains";
 import {ActionPayload, ActionResponse} from "../../../shared/service/types";
+import {APIGatewayProxyHandlerV2} from "aws-lambda";
 
 export const Name = 'search';
 
@@ -14,3 +15,16 @@ export class Response implements ActionResponse {
 export const Handler = async (payload: Payload) => {
   return new Response();
 }
+
+const LambdaHandler: APIGatewayProxyHandlerV2 = async (event) => {
+  const response = await Handler({
+    terms: event.queryStringParameters.terms,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  }
+}
+
+export default LambdaHandler;
