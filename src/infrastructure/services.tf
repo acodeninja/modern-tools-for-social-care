1cb75bc9956facac1c9e0d0e61f8d9531fd1b1d8
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-02 12:43:11 +0000

    Correction to instance type for search

--- src/infrastructure/services.tf
+++ src/infrastructure/services.tf
@@ -2,6 +2,6 @@ module "search_service" {
   source                           = "../services/search/infrastructure/account"
   environment                      = var.environment
   system                           = var.system
-  aws_elasticsearch_instance_type  = "t3.small.search"
+  aws_elasticsearch_instance_type  = "t3.small.elasticsearch"
   aws_elasticsearch_instance_count = 1
 }
