8485d21068fb1a28a3297b5549b27752f9f3ee52
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-04 13:41:35 +0000

    Lambdify now decodes the body for a payload

--- src/services/search/lib/lambda.ts
+++ src/services/search/lib/lambda.ts
@@ -2,9 +2,11 @@ import {APIGatewayProxyHandlerV2} from "aws-lambda";
 import {ActionManifest} from "../../../shared/service/types";
 
 export const LambdifyHandler = (Handler: ActionManifest['Handler']): APIGatewayProxyHandlerV2 => async (event) => {
-  const response = await Handler({
-    terms: event.queryStringParameters.terms,
-  });
+  const response = await Handler(JSON.parse(
+    event.isBase64Encoded ?
+      Buffer.from(event.body, 'base64').toString('ascii') :
+      event.body
+  ));
 
   return {
     statusCode: 200,
