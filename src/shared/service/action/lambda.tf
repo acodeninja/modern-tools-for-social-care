347e665a781432265fe2d040891b020cae5f637c
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-04 12:06:18 +0000

    Fix environment block with variables set

--- src/shared/service/action/lambda.tf
+++ src/shared/service/action/lambda.tf
@@ -3,8 +3,11 @@ resource "aws_lambda_function" "action" {
   role          = aws_iam_role.action_role.arn
   handler       = var.handler
   runtime       = var.runtime
-  environment   = var.environment
   publish       = true
   s3_bucket     = local.manifest.deploy.bucket
   s3_key        = "${var.service}/actions/${var.action}.zip"
+
+  environment {
+    variables = var.environment_variables
+  }
 }
