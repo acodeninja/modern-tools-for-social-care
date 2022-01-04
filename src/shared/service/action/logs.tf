6718ebf6c7b38b354bda584abcc4a08052cc1fae
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-04 16:03:34 +0000

    Further attempts at correctly naming log groups

--- src/shared/service/action/logs.tf
+++ src/shared/service/action/logs.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "action" {
-  name              = "/aws/lambda/${var.system}-${var.environment}/${var.service}/action/${var.action}"
+  name              = "/aws/lambda/${aws_lambda_function.action.function_name}"
   retention_in_days = var.log_retention_days
 }
