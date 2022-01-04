e9ec80e9a503131e7d83d5a9793699279fcd33be
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-04 13:12:08 +0000

    Specify lambda deployment version

--- src/shared/service/action/lambda.tf
+++ src/shared/service/action/lambda.tf
@@ -1,11 +1,12 @@
 resource "aws_lambda_function" "action" {
-  function_name = "${var.system}-${var.environment}-${var.service}-action-${var.action}"
-  role          = aws_iam_role.action_role.arn
-  handler       = var.handler
-  runtime       = var.runtime
-  publish       = true
-  s3_bucket     = local.manifest.deploy.bucket
-  s3_key        = "${var.service}/actions/${var.action}.zip"
+  function_name     = "${var.system}-${var.environment}-${var.service}-action-${var.action}"
+  role              = aws_iam_role.action_role.arn
+  handler           = var.handler
+  runtime           = var.runtime
+  publish           = true
+  s3_bucket         = local.manifest.deploy.bucket
+  s3_key            = "${var.service}/actions/${var.action}.zip"
+  s3_object_version = aws_s3_bucket_object.artefact.version_id
 
   environment {
     variables = var.environment_variables
