afa04389eaceb2fde2f814deeb2643d543fde31f
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-04 19:26:32 +0000

    Add search action lambda deploy

--- src/services/search/infrastructure/actions.tf
+++ src/services/search/infrastructure/actions.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "put_to_open_search" {
   statement {
-    sid = "AllowPuttingToOpenSearch"
+    sid    = "AllowPuttingToOpenSearch"
     effect = "Allow"
     actions = [
       "es:ESHttpPost"
@@ -22,6 +22,37 @@ module "action-update" {
   artefacts   = "${path.root}/../build/actions/update"
   runtime     = "nodejs14.x"
   policy      = data.aws_iam_policy_document.put_to_open_search.json
+  environment_variables = {
+    ENVIRONMENT             = var.environment
+    SYSTEM                  = var.system
+    AWS_OPENSEARCH_ENDPOINT = local.account-manifest.opensearch.endpoint
+  }
+}
+
+data "aws_iam_policy_document" "get_to_open_search" {
+  statement {
+    sid    = "AllowGettingFromOpenSearch"
+    effect = "Allow"
+    actions = [
+      "es:ESHttpGet"
+    ]
+    resources = [
+      local.account-manifest.opensearch.arn,
+      "${local.account-manifest.opensearch.arn}/*",
+    ]
+  }
+}
+
+module "action-search" {
+  source      = "../../../shared/service/action"
+  system      = var.system
+  environment = var.environment
+  service     = "search"
+  action      = "search"
+  handler     = "search.LambdaHandler"
+  artefacts   = "${path.root}/../build/actions/search"
+  runtime     = "nodejs14.x"
+  policy      = data.aws_iam_policy_document.get_to_open_search.json
   environment_variables = {
     ENVIRONMENT             = var.environment
     SYSTEM                  = var.system
