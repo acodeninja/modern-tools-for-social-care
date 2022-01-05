c82ea7db3100dfad66dc10c31395fce8fde53c5c
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-05 14:09:29 +0000

    Try to get files listing from github api

--- .github/workflows/scripts/build-matrix.js
+++ .github/workflows/scripts/build-matrix.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = () => new Promise(resolve => {
-  const mainInfra = path.resolve(__dirname, '..', '..', '..', 'src', 'infrastructure');
+module.exports = async ({github, context}) => {
+  const possibleChanges = [path.resolve(__dirname, '..', '..', '..', 'src', 'infrastructure')] +
+    fs.readdirSync(path.resolve(__dirname, '..', '..', '..', 'src', 'services'))
+      .map(service => path.resolve(__dirname, '..', '..', '..', 'src', 'services', service));
 
-  const services = fs.readdirSync(path.resolve(__dirname, '..', '..', '..', 'src', 'services'))
-    .map(service => path.resolve(__dirname, '..', '..', '..', 'src', 'services', service));
+  const [owner, repo] = context.payload.repository.full_name.split('/');
+  const pull_number = context.payload.pull_request.number;
 
-  console.log(services);
-  resolve();
-});
+  const actualChanges = github.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number,
+  });
+
+  console.log(possibleChanges, actualChanges);
+};
