bd6046876d158901fca97c455ad8363f84fe2060
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-05 14:18:30 +0000

    Extract some methods to functions

--- .github/workflows/scripts/build-matrix.js
+++ .github/workflows/scripts/build-matrix.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = async ({github, context}) => {
-  const possibleChanges = [path.resolve(__dirname, '..', '..', '..', 'src', 'infrastructure')] +
-    fs.readdirSync(path.resolve(__dirname, '..', '..', '..', 'src', 'services'))
-      .map(service => path.resolve(__dirname, '..', '..', '..', 'src', 'services', service));
+const getPossibleChanges = () => [
+  path.resolve(__dirname, '..', '..', '..', 'src', 'infrastructure'),
+  fs.readdirSync(path.resolve(__dirname, '..', '..', '..', 'src', 'services'))
+    .map(service => path.resolve(__dirname, '..', '..', '..', 'src', 'services', service)),
+].flat();
 
+const getActualChanges = async ({github, context}) => {
   const [owner, repo] = context.payload.repository.full_name.split('/');
   const pull_number = context.payload.pull_request.number;
 
-  const actualChanges = (await github.rest.pulls.listFiles({
-    owner,
-    repo,
-    pull_number,
-  })).data.map(file => file.filename);
+  return (await github.rest.pulls.listFiles({owner, repo, pull_number})).data.map(file => file.filename);
+}
+
+module.exports = async ({github, context}) => {
+  const possibleChanges = getPossibleChanges();
+  const actualChanges = await getActualChanges({github, context});
 
   console.log(possibleChanges, actualChanges);
 };
