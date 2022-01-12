660b59e887bdf43e9302201041562408c07d5e9b
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-12 19:07:31 +0000

    Be more accepting of bad payloads on lambda functions

--- src/services/search/lib/lambda.ts
+++ src/services/search/lib/lambda.ts
@@ -1,5 +1,9 @@
-import {APIGatewayProxyHandlerV2} from "aws-lambda";
-import {ActionManifest} from "../../../framework/service/types";
+import {
+  APIGatewayEventRequestContextV2,
+  APIGatewayProxyEventV2WithRequestContext,
+  APIGatewayProxyHandlerV2
+} from "aws-lambda";
+import {ActionManifest, ActionPayload} from "../../../framework/service/types";
 
 export class RequestError extends Error {
 
@@ -9,12 +13,28 @@ export class ServerError extends Error {
 
 }
 
+export const LambdaExtractPayload = (event: APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>): ActionPayload => {
+  let payload = {};
+
+  try {
+    payload = Object.assign(payload, JSON.parse(
+      event.isBase64Encoded ?
+        Buffer.from(event.body, 'base64').toString('ascii') :
+        event.body
+    ));
+  } catch (e) {
+  }
+
+  try {
+    payload = Object.assign(payload, event.queryStringParameters);
+  } catch (e) {
+  }
+
+  return payload;
+}
+
 export const LambdifyHandler = (Handler: ActionManifest['Handler']): APIGatewayProxyHandlerV2 => async (event) => {
-  const payload = Object.assign(JSON.parse(
-    event.isBase64Encoded ?
-      Buffer.from(event.body, 'base64').toString('ascii') :
-      event.body
-  ), event.queryStringParameters);
+  const payload = LambdaExtractPayload(event);
 
   try {
     const response = await Handler(payload);
