0533152ed4a4c65a192d6683c379301430426694
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-12 15:31:07 +0000

    Allow query string params to override request body for payload

--- src/services/search/lib/lambda.ts
+++ src/services/search/lib/lambda.ts
@@ -2,11 +2,13 @@ import {APIGatewayProxyHandlerV2} from "aws-lambda";
 import {ActionManifest} from "../../../framework/service/types";
 
 export const LambdifyHandler = (Handler: ActionManifest['Handler']): APIGatewayProxyHandlerV2 => async (event) => {
-  const response = await Handler(JSON.parse(
+  const payload = Object.assign(JSON.parse(
     event.isBase64Encoded ?
       Buffer.from(event.body, 'base64').toString('ascii') :
       event.body
-  ));
+  ), event.queryStringParameters);
+
+  const response = await Handler(payload);
 
   return {
     statusCode: 200,
