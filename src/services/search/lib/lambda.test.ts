affff8348fce110d578cfc19ca578d9c2aeca3bd
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-19 12:21:25 +0000

    Test lambda payload that is base64 encoded

--- src/services/search/lib/lambda.test.ts
+++ src/services/search/lib/lambda.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test} from '@jest/globals';
 import {LambdaExtractPayload, RequestError} from "./lambda";
 import {APIGatewayEventRequestContextV2, APIGatewayProxyEventV2WithRequestContext} from "aws-lambda";
 
-describe('services/search/lib/lambda/LambdaExtractPayload', () => {
+describe('services/search/lib/lambda.LambdaExtractPayload', () => {
   describe('when there is no payload', () => {
     test('returns an empty object', () => {
       expect(LambdaExtractPayload({
@@ -11,7 +11,6 @@ describe('services/search/lib/lambda/LambdaExtractPayload', () => {
       } as unknown as APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>)).toEqual({});
     });
   });
-
 
   describe('when the payload is in the query string', () => {
     test('parses the payload', () => {
@@ -38,7 +37,7 @@ describe('services/search/lib/lambda/LambdaExtractPayload', () => {
     });
   });
 
-  describe('when the payload is in the body', () => {
+  describe('when the payload is a string in the body', () => {
     test('parses a valid payload', () => {
       expect(LambdaExtractPayload({
         body: JSON.stringify({test: 'test'}),
@@ -50,6 +49,24 @@ describe('services/search/lib/lambda/LambdaExtractPayload', () => {
     test('throws a request error when the body is not JSON', () => {
       expect(() => LambdaExtractPayload({
         body: '{test: "test"}',
+      } as unknown as APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>))
+        .toThrow(new RequestError("The request body is malformed."));
+    });
+  });
+  describe('when the payload is base64 encoded in the body', () => {
+    test('parses a valid payload', () => {
+      expect(LambdaExtractPayload({
+        body: Buffer.from(JSON.stringify({test: 'test'})).toString('base64'),
+        isBase64Encoded: true,
+      } as unknown as APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>)).toEqual({
+        test: 'test',
+      });
+    });
+
+    test('throws a request error when the body is not JSON', () => {
+      expect(() => LambdaExtractPayload({
+        body: Buffer.from('{test: "test"}').toString('base64'),
+        isBase64Encoded: true,
       } as unknown as APIGatewayProxyEventV2WithRequestContext<APIGatewayEventRequestContextV2>))
         .toThrow(new RequestError("The request body is malformed."));
     });
