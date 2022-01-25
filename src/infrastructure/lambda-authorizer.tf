a1125cf56d3c77072f303a0243948a4beafb8296
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-25 16:03:45 +0000

    First stab at a lambda authorizer function

---
+++ src/infrastructure/lambda-authorizer.tf
@@ -0,0 +1,74 @@
+resource "aws_cloudwatch_log_group" "authorizer" {
+  name              = "/aws/lambda/${var.system}-${var.environment}-authorizer"
+  retention_in_days = 14
+}
+
+data "aws_iam_policy_document" "authorizer_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "authorizer_role" {
+  name               = "${var.system}-${var.environment}-authorizer-role"
+  assume_role_policy = data.aws_iam_policy_document.authorizer_assume_role.json
+}
+
+data "aws_iam_policy_document" "action_logging" {
+  statement {
+    sid    = "AllowLoggingToCloudwatch"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      aws_cloudwatch_log_group.authorizer.arn,
+      "${aws_cloudwatch_log_group.authorizer.arn}:*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda" {
+  name   = "${var.system}-${var.environment}-authorizer-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.action_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.authorizer_role.name
+  policy_arn = aws_iam_policy.lambda.arn
+}
+
+resource "aws_lambda_function" "authorizer" {
+  function_name     = "${var.system}-${var.environment}-authorizer"
+  role              = aws_iam_role.authorizer_role.arn
+  handler           = 'lambda-authorizer.handler'
+  runtime           = "nodejs14.x"
+  publish           = true
+  s3_bucket         = aws_s3_bucket_object.artefact.bucket
+  s3_key            = aws_s3_bucket_object.artefact.key
+  s3_object_version = aws_s3_bucket_object.artefact.version_id
+
+  depends_on = [aws_s3_bucket_object.artefact]
+}
+
+data "archive_file" "action" {
+  output_path = "${path.root}/dist/lambda-authorizer.zip"
+  type        = "zip"
+  source_dir  = "${path.root}/build/lambda-authorizer"
+}
+
+resource "aws_s3_bucket_object" "artefact" {
+  bucket       = aws_s3_bucket.deployments.bucket
+  key          = "lambda-authorizer.zip"
+  content_type = "application/zip"
+  source       = data.archive_file.action.output_path
+  source_hash  = data.archive_file.action.output_md5
+}
