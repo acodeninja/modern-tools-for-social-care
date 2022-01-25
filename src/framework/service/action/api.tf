4a87e6e33818e4dd011f751ebbdaceb087269706
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-25 12:27:54 +0000

    Add a nice name to the action api integration

--- src/framework/service/action/api.tf
+++ src/framework/service/action/api.tf
@@ -22,7 +22,7 @@ resource "aws_apigatewayv2_integration" "action" {
   api_id               = var.api_id
   integration_type     = "AWS_PROXY"
   connection_type      = "INTERNET"
-  description          = "Search service API"
+  description          = "${var.action} API"
   integration_method   = "POST"
   integration_uri      = aws_lambda_function.action.invoke_arn
   passthrough_behavior = "WHEN_NO_MATCH"
