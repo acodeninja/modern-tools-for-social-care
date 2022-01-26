5f07e9a6b4da2c6bd04ca64e3eeb01e7230b94f4
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-26 11:51:30 +0000

    Send a test token through from the authorizer

--- src/infrastructure/lambda-authorizer.ts
+++ src/infrastructure/lambda-authorizer.ts
@@ -4,6 +4,18 @@ import {inspect} from "util";
 export const handler: APIGatewayRequestAuthorizerHandler = async (event) => {
   console.log(inspect(event, false, 15));
 
+  const testToken = {
+    sub: "100561961286081451085",
+    email: "test@example.com",
+    iss: "Example Authority",
+    name: "Test Testington",
+    groups: [
+      "developers",
+      "search-admin"
+    ],
+    iat: Math.floor(Date.now() / 1000),
+  };
+
   return {
     principalId: 'user',
     policyDocument: {
@@ -14,5 +26,8 @@ export const handler: APIGatewayRequestAuthorizerHandler = async (event) => {
         Resource: event.methodArn,
       }],
     },
+    context: {
+      token: Buffer.from(JSON.stringify(testToken)).toString('base64url'),
+    }
   };
 };
