d7591641518223c0997f95eb77da05541ff24024
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-26 14:27:44 +0000

    Use the ids of resources to trigger deployment

--- src/framework/service/action/outputs.tf
+++ src/framework/service/action/outputs.tf
@@ -1,7 +1,7 @@
 output "api-config" {
   value = {
-    route       = aws_apigatewayv2_route.action
-    integration = aws_apigatewayv2_integration.action
-    authorizer  = aws_apigatewayv2_authorizer.authorizer
+    route       = aws_apigatewayv2_route.action.id
+    integration = aws_apigatewayv2_integration.action.id
+    authorizer  = aws_apigatewayv2_authorizer.authorizer[0].id
   }
 }
