6a6b82bb809101b534936cc06f5f7fcc5e69df85
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-26 14:34:14 +0000

    Attempt to use for to extract authorizers

--- src/framework/service/action/outputs.tf
+++ src/framework/service/action/outputs.tf
@@ -2,6 +2,6 @@ output "api-config" {
   value = {
     route       = aws_apigatewayv2_route.action.id
     integration = aws_apigatewayv2_integration.action.id
-    authorizer  = aws_apigatewayv2_authorizer.authorizer[0].id
+    authorizer  = [ for auth in aws_apigatewayv2_authorizer.authorizer : auth.id ]
   }
 }
