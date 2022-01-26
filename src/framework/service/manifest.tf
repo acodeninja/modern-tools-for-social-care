9fef2865fb5efd39d7ea808bc298e74443089a08
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-26 17:54:19 +0000

    Add manifest for each service

---
+++ src/framework/service/manifest.tf
@@ -0,0 +1,19 @@
+locals {
+  manifest-content = base64encode(jsonencode({
+    actions = {
+    for action in var.config.actions : action.name => {
+
+    }
+    }
+    events  = {}
+    subscribers = {}
+    views = {}
+  }))
+}
+
+resource "aws_s3_bucket_object" "manifest" {
+  bucket         = "${var.system}-${var.environment}-manifests"
+  key            = "services/${var.name}.json"
+  content_type   = "application/json"
+  content_base64 = local.manifest-content
+}
