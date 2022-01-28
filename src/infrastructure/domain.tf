68ca83e3d4b097b24d06ac2c7920454eec0aa53b
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-28 16:35:11 +0000

    Add wildcard cert for root domain

--- src/infrastructure/domain.tf
+++ src/infrastructure/domain.tf
@@ -1,3 +1,26 @@
 resource "aws_route53_zone" "root" {
   name = var.root-domain
 }
+
+resource "aws_acm_certificate" "root" {
+  domain_name               = var.root-domain
+  subject_alternative_names = ["*.${var.root-domain}"]
+  validation_method         = "DNS"
+}
+
+resource "aws_route53_record" "example" {
+  for_each = {
+  for option in aws_acm_certificate.root.domain_validation_options : option.domain_name => {
+    name   = option.resource_record_name
+    record = option.resource_record_value
+    type   = option.resource_record_type
+  }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.root.zone_id
+}
