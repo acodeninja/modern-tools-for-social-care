3c70c6151152154239be841ea500eee288bcf05d
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-01-28 18:15:30 +0000

    Map API Gateway to the service domain

---
+++ src/framework/service/domain.tf
@@ -0,0 +1,35 @@
+data "aws_route53_zone" "root" {
+  name = var.root-domain
+}
+
+data "aws_acm_certificate" "root" {
+  domain = var.root-domain
+}
+
+resource "aws_apigatewayv2_domain_name" "service" {
+  domain_name = "${var.name}.${var.root-domain}"
+
+  domain_name_configuration {
+    certificate_arn = data.aws_acm_certificate.root.arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "service" {
+  name    = aws_apigatewayv2_domain_name.service.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.root.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.service.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.service.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "service" {
+  api_id      = aws_apigatewayv2_api.api.id
+  domain_name = aws_apigatewayv2_domain_name.service.id
+  stage       = aws_apigatewayv2_stage.api.id
+}
